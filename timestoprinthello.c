62f09ab158d12e5543b4d402ebced42aa010ec23
vasanth <35736389+mappillaivasanth@users.noreply.github.com>
2018-02-01 16:43:36 +0530

    Create timestoprinthello.c

---
+++ timestoprinthello.c
@@ -0,0 +1,11 @@
+int main()
+{
+    int a,i;
+    printf("Enter the number that you want to print 'Hello' :");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        printf("Hello\n");
+    }
+    return 0;
+}
